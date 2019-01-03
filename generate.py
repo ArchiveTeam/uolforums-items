eb055821f4d5a3e05cffed02483c1c193f679fd5
Arkiver2 <arkiver@protonmail.com>
2019-01-03 22:02:56 +0100

    Threads 0 to 999999 added.

---
+++ generate.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main(start, end):
+    assert start % 10 == 0
+    assert end % 10 == 9
+    with open('jogos_{}-{}.txt'.format(start, end), 'w') as f:
+        f.write('\n'.join(['jogos:{}-{}'.format(a, a+9)
+                           for a in range(start, end, 10)]))
+
+if __name__ == '__main__':
+    main(*[int(i) for i in sys.argv[1:]])
+
