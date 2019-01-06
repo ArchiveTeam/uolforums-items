c6d552e8a04af2dd4930545da9ad50e05dae3720
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2019-01-06 06:25:02 +0100

    Fix newline at end of list

--- generate.py
+++ generate.py
@@ -7,6 +7,7 @@ def main(start, end):
     with open('jogos_{}-{}.txt'.format(start, end), 'w') as f:
         f.write('\n'.join(['jogos:{}-{}'.format(a, a+9)
                            for a in range(start, end, 10)]))
+        f.write('\n')
 
 if __name__ == '__main__':
     main(*[int(i) for i in sys.argv[1:]])
