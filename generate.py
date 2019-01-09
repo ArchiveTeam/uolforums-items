24657e25ef24ea361d54ee6576cc915bfeac0b03
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2019-01-09 17:35:47 +0100

    Adapt generator script for other forums

--- generate.py
+++ generate.py
@@ -1,14 +1,13 @@
 import sys
 
 
-def main(start, end):
+def main(type_, start, end):
     assert start % 10 == 0
     assert end % 10 == 9
-    with open('jogos_{}-{}.txt'.format(start, end), 'w') as f:
-        f.write('\n'.join(['jogos:{}-{}'.format(a, a+9)
-                           for a in range(start, end, 10)]))
+    with open('{}_{}-{}.txt'.format(type_, start, end), 'w') as f:
+        f.write('\n'.join('{}:{}-{}'.format(type_, a, a+9) for a in range(start, end, 10)))
         f.write('\n')
 
 if __name__ == '__main__':
-    main(*[int(i) for i in sys.argv[1:]])
+    main(sys.argv[1], *[int(i) for i in sys.argv[2:]])
 
